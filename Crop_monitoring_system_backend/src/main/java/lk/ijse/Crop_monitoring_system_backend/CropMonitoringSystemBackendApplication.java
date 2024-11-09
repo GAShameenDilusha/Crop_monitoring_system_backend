8e9277307f7d15e3890ba1186980dbc20fd117b2
shameen <shameendilusha5678@gmail.com>
2024-11-09 15:54:54 +0530

    Initial commit

---
+++ Crop_monitoring_system_backend/src/main/java/lk/ijse/Crop_monitoring_system_backend/CropMonitoringSystemBackendApplication.java
@@ -0,0 +1,13 @@
+package lk.ijse.Crop_monitoring_system_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CropMonitoringSystemBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CropMonitoringSystemBackendApplication.class, args);
+	}
+
+}
