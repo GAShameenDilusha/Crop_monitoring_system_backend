06fa6adae7ad1969509811177b84271b905a4d24
shameen <shameendilusha5678@gmail.com>
2024-11-13 21:38:29 +0530

    Updated

--- Crop_monitoring_system_backend/src/main/java/lk/ijse/Crop_monitoring_system_backend/service/FieldService.java
+++ Crop_monitoring_system_backend/src/main/java/lk/ijse/Crop_monitoring_system_backend/service/FieldService.java
@@ -1,4 +1,11 @@
 package lk.ijse.Crop_monitoring_system_backend.service;
 
-public interface FieldService {
+
+import lk.ijse.Crop_monitoring_system_backend.dto.FieldDTO;
+
+public interface FieldService extends SuperService<String, FieldDTO>{
+    String getFieldCode();
+
+
+    String getFieldName(String fieldCode);
 }
