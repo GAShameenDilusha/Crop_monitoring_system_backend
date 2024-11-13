726ac4e82bb0088c1f435bd5f06dfd4bccb7a1fb
shameen <shameendilusha5678@gmail.com>
2024-11-13 21:35:44 +0530

    update FieldController

--- Crop_monitoring_system_backend/src/main/java/lk/ijse/Crop_monitoring_system_backend/controller/FieldController.java
+++ Crop_monitoring_system_backend/src/main/java/lk/ijse/Crop_monitoring_system_backend/controller/FieldController.java
@@ -1,4 +1,11 @@
 package lk.ijse.Crop_monitoring_system_backend.controller;
 
-public interface FieldController {
+import java.util.List;
+
+public interface FieldController<ID,T> extends SuperController{
+    public boolean saveField(T fieldDTO);
+    public boolean updateField(T fieldDTO);
+    public boolean deleteField(ID field_code);
+    public T searchField(ID field_code);
+    public List<T> getAllFieldes();
 }
