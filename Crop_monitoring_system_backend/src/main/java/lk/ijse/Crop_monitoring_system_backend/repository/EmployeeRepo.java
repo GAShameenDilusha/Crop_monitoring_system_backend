35eeb0c04cf9f36e53a389f7e05f065efce77f28
shameen <shameendilusha5678@gmail.com>
2024-11-16 21:14:56 +0530

    update EmployeeRepo

--- Crop_monitoring_system_backend/src/main/java/lk/ijse/Crop_monitoring_system_backend/repository/EmployeeRepo.java
+++ Crop_monitoring_system_backend/src/main/java/lk/ijse/Crop_monitoring_system_backend/repository/EmployeeRepo.java
@@ -1,3 +1,4 @@
+/*
 package lk.ijse.Crop_monitoring_system_backend.repository;
 
 
@@ -24,4 +25,28 @@ public interface EmployeeRepo extends JpaRepository<EmployeeEntity, String> {
 
     @Query(value = "SELECT branch_code FROM employee WHERE employee_code = ?1", nativeQuery = true)
     String getBranchCodeByEmployeeCode(String cashierName);
+}
+*/
+package lk.ijse.Crop_monitoring_system_backend.repository;
+
+import lk.ijse.Crop_monitoring_system_backend.entity.EmployeeEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface EmployeeRepo extends JpaRepository<EmployeeEntity, String> {
+    @Query(value = "SELECT id FROM staff ORDER BY id DESC LIMIT 1", nativeQuery = true)
+    String getEmployeeCode();
+
+    @Query(value = "SELECT DISTINCT designation FROM staff", nativeQuery = true)
+    List<String> findDistinctDesignations();
+
+    @Query(value = "SELECT field FROM staff_fields WHERE staff_id = ?1", nativeQuery = true)
+    List<String> findFieldsByEmployeeId(String employeeId);
+
+    @Query(value = "SELECT vehicle FROM staff_vehicles WHERE staff_id = ?1", nativeQuery = true)
+    List<String> findVehiclesByEmployeeId(String employeeId);
 }
