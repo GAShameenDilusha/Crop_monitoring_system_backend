887946ce249fe91528be023989f9f8646a629e63
shameen <shameendilusha5678@gmail.com>
2024-11-16 21:14:08 +0530

    update EmployeeController interface

--- Crop_monitoring_system_backend/src/main/java/lk/ijse/Crop_monitoring_system_backend/controller/EmployeeController.java
+++ Crop_monitoring_system_backend/src/main/java/lk/ijse/Crop_monitoring_system_backend/controller/EmployeeController.java
@@ -1,3 +1,4 @@
+/*
 package lk.ijse.Crop_monitoring_system_backend.controller;
 
 import java.util.List;
@@ -8,4 +9,16 @@ public interface EmployeeController<ID,T> extends SuperController{
     public boolean deleteEmployee(ID employee_code);
     public T searchEmployee(ID employee_code);
     public List<T> getAllEmployees();
+}
+*/
+package lk.ijse.Crop_monitoring_system_backend.controller;
+
+import java.util.List;
+
+public interface EmployeeController<ID,T> extends SuperController {
+    boolean saveEmployee(T employeeDTO);
+    boolean updateEmployee(T employeeDTO);
+    boolean deleteEmployee(ID id);
+    T searchEmployee(ID id);
+    List<T> getAllEmployees();
 }
