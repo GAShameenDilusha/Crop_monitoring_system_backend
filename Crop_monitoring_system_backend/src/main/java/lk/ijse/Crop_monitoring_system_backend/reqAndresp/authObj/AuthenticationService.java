4efde42eead7c6e6e091a510f4fb19563d5e19c6
shameen <shameendilusha5678@gmail.com>
2024-11-25 12:57:31 +0530

    update AuthenticationService

--- Crop_monitoring_system_backend/src/main/java/lk/ijse/Crop_monitoring_system_backend/reqAndresp/authObj/AuthenticationService.java
+++ Crop_monitoring_system_backend/src/main/java/lk/ijse/Crop_monitoring_system_backend/reqAndresp/authObj/AuthenticationService.java
@@ -4,10 +4,6 @@ package lk.ijse.Crop_monitoring_system_backend.reqAndresp.authObj;
 import lk.ijse.Crop_monitoring_system_backend.reqAndresp.response.JwtAuthResponse;
 import lk.ijse.Crop_monitoring_system_backend.reqAndresp.secure.SignIn;
 import lk.ijse.Crop_monitoring_system_backend.reqAndresp.secure.SignUp;
-import lk.ijse.gdse.hello_shoe_pvt_ltd.reqAndresp.response.JwtAuthResponse;
-import lk.ijse.gdse.hello_shoe_pvt_ltd.reqAndresp.secure.SignIn;
-import lk.ijse.gdse.hello_shoe_pvt_ltd.reqAndresp.secure.SignUp;
-
 public interface AuthenticationService {
     JwtAuthResponse signIn(SignIn signIn);
     JwtAuthResponse signUp(SignUp signIn);
