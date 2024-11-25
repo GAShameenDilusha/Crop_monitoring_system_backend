db9f4bb0d387e80d27a41518c7cd47ee58c71b0d
shameen <shameendilusha5678@gmail.com>
2024-11-25 12:59:14 +0530

    update UserDTO.java

--- Crop_monitoring_system_backend/src/main/java/lk/ijse/Crop_monitoring_system_backend/dto/UserDTO.java
+++ Crop_monitoring_system_backend/src/main/java/lk/ijse/Crop_monitoring_system_backend/dto/UserDTO.java
@@ -1,4 +1,20 @@
 package lk.ijse.Crop_monitoring_system_backend.dto;
 
-public class UserDTO {
+
+import lk.ijse.Crop_monitoring_system_backend.util.enums.Role;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Builder
+public class UserDTO implements SuperDTO {
+    private String id;
+    private String email;
+    private String password;
+    private Role role;
+    private String employee_code;
 }
