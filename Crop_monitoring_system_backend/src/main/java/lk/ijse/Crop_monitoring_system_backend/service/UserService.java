41139188f0bc58bd31c943bdff25e538be8d098d
shameen <shameendilusha5678@gmail.com>
2024-11-25 12:59:48 +0530

    update UserService

--- Crop_monitoring_system_backend/src/main/java/lk/ijse/Crop_monitoring_system_backend/service/UserService.java
+++ Crop_monitoring_system_backend/src/main/java/lk/ijse/Crop_monitoring_system_backend/service/UserService.java
@@ -1,4 +1,19 @@
 package lk.ijse.Crop_monitoring_system_backend.service;
 
+import lk.ijse.Crop_monitoring_system_backend.dto.UserDTO;
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+import java.util.List;
+
 public interface UserService {
+    UserDetailsService userDetailsService();
+    void saveUser(UserDTO userDTO);
+
+    String getRole(String email);
+
+    String getEmployeeCode(String cashierName);
+
+    List<UserDTO> getAllUsers();
+
+    boolean deleteUser(String email);
 }
