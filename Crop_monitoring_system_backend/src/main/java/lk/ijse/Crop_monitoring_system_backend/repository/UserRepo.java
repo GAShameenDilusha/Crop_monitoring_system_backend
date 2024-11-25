3dc6f8e48a4c98e089fe185ce0b7d9feacc26097
shameen <shameendilusha5678@gmail.com>
2024-11-25 12:59:32 +0530

    update UserRepo

--- Crop_monitoring_system_backend/src/main/java/lk/ijse/Crop_monitoring_system_backend/repository/UserRepo.java
+++ Crop_monitoring_system_backend/src/main/java/lk/ijse/Crop_monitoring_system_backend/repository/UserRepo.java
@@ -1,4 +1,21 @@
 package lk.ijse.Crop_monitoring_system_backend.repository;
 
-public interface UserRepo {
+import jakarta.transaction.Transactional;
+
+import lk.ijse.Crop_monitoring_system_backend.entity.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepo extends JpaRepository<UserEntity,String> {
+    Optional<UserEntity> findByEmail(String email);
+
+    @Modifying
+    @Transactional
+    @Query(value = "DELETE FROM user WHERE email = ?1", nativeQuery = true)
+    void deleteByEmail(String email);
 }
