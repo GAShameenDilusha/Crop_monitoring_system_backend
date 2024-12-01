4489d6bc7b1176e41118ec671b7593cdccc494b2
shameen <shameendilusha5678@gmail.com>
2024-12-01 21:55:29 +0530

    update CropEntity.java

---
+++ Crop_monitoring_system_backend/src/main/java/lk/ijse/Crop_monitoring_system_backend/entity/CropEntity.java
@@ -0,0 +1,33 @@
+package lk.ijse.Crop_monitoring_system_backend.Entity;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Entity
+@Table(name = "crop")
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class CropEntity {
+    @Id
+    private String cropId;
+
+    private String commonName;
+    private String specificName;
+    private String category;
+    private String season;
+    @Column(columnDefinition = "LONGTEXT")
+    private String image1;
+
+    @ManyToOne
+    @JoinColumn(name = "field_id")
+    private FieldEntity field;
+    @ManyToMany(mappedBy = "cropLogs")
+    @JsonBackReference
+    private List<LogEntity> logs;
+}
