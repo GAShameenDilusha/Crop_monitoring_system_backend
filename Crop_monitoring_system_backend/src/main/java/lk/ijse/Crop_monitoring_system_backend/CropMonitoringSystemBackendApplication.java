70881c7be6de6dcfd7369b4a439944026b042a17
shameen <shameendilusha5678@gmail.com>
2024-12-01 21:55:51 +0530

    update CropMonitoringSystemBackendApplication.java

--- Crop_monitoring_system_backend/src/main/java/lk/ijse/Crop_monitoring_system_backend/CropMonitoringSystemBackendApplication.java
+++ Crop_monitoring_system_backend/src/main/java/lk/ijse/Crop_monitoring_system_backend/CropMonitoringSystemBackendApplication.java
@@ -1,13 +1,26 @@
 package lk.ijse.Crop_monitoring_system_backend;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
+@EnableWebSecurity
+@EnableMethodSecurity
 public class CropMonitoringSystemBackendApplication {
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(CropMonitoringSystemBackendApplication.class, args);
+	}
+	@Bean
+	public ModelMapper modelMapper(){
+		return new ModelMapper();
 	}
 
 }
