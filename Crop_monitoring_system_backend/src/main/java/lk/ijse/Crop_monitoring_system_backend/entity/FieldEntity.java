ab9622abee28f25bf869ebabb0d1387f43929c89
shameen <shameendilusha5678@gmail.com>
2024-12-02 12:38:19 +0530

    update FieldEntity

--- Crop_monitoring_system_backend/src/main/java/lk/ijse/Crop_monitoring_system_backend/entity/FieldEntity.java
+++ Crop_monitoring_system_backend/src/main/java/lk/ijse/Crop_monitoring_system_backend/entity/FieldEntity.java
@@ -1,69 +1,43 @@
-package lk.ijse.Crop_monitoring_system_backend.entity;
+package lk.ijse.Crop_monitoring_system_backend.Entity;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.awt.*;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
-@NoArgsConstructor
-@AllArgsConstructor
-@Data
 @Entity
 @Table(name = "field")
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 public class FieldEntity {
     @Id
-    private String field_code;
+    private String FieldId;
+    private String name;
+    private String location;
+    private Double size;
+    @Column(columnDefinition = "LONGTEXT")
+    private String image1;
+    @Column(columnDefinition = "LONGTEXT")
+    private String image2;
 
-    private String field_name;
+    @ManyToMany(mappedBy = "fields")
+    @JsonBackReference
+    private Set<StaffEntity> staffMembers = new HashSet<>();
 
-    @Embedded
-    private Point field_location;
-
-    private Double extent_size_of_the_Field;
-
-    @ElementCollection
-    @CollectionTable(name = "field_crops", joinColumns = @JoinColumn(name = "field_code"))
-    @Column(name = "crop")
-    private List<String> crops;
-
-    @ElementCollection
-    @CollectionTable(name = "field_staff", joinColumns = @JoinColumn(name = "field_code"))
-    @Column(name = "staff_member")
-    private List<String> staff;
-
-    @Column(columnDefinition = "TEXT")
-    private String field_image1;
-
-    @Column(columnDefinition = "TEXT")
-    private String field_image2;
+    @OneToMany(mappedBy = "field", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<CropEntity> crops ;
+    @ManyToMany(mappedBy = "fieldLogs")
+    @JsonBackReference
+    private List<LogEntity> logs;
+    @OneToMany(mappedBy = "field", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<EquipmentEntity> equipment;
 
 
 
-
-    // Additional Point class for GPS coordinates
-    @Embeddable
-    class Point {
-        private Double latitude;
-        private Double longitude;
-
-        // Getters and setters
-        public Double getLatitude() {
-            return latitude;
-        }
-
-        public void setLatitude(Double latitude) {
-            this.latitude = latitude;
-        }
-
-        public Double getLongitude() {
-            return longitude;
-        }
-
-        public void setLongitude(Double longitude) {
-            this.longitude = longitude;
-        }
-    }
 }
