98e598c68c26abb07ded87adfb819c20a95d6252
shameen <shameendilusha5678@gmail.com>
2024-12-02 12:46:36 +0530

    update SignIn.java

--- Crop_monitoring_system_backend/src/main/java/lk/ijse/Crop_monitoring_system_backend/Secure/SignIn.java
+++ Crop_monitoring_system_backend/src/main/java/lk/ijse/Crop_monitoring_system_backend/Secure/SignIn.java
@@ -1,12 +1,12 @@
-package lk.ijse.Crop_monitoring_system_backend.reqAndresp.secure;
+package lk.ijse.Crop_monitoring_system_backend.Secure;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@NoArgsConstructor
 @AllArgsConstructor
+@NoArgsConstructor
 @Data
 @Builder
 public class SignIn {
