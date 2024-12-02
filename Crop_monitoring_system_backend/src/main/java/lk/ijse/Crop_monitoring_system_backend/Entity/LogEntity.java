4b913285b1db14ad1bb3e93cf08ec2ba95c76cb5
shameen <shameendilusha5678@gmail.com>
2024-12-02 12:41:41 +0530

    update LogEntity.java

---
+++ Crop_monitoring_system_backend/src/main/java/lk/ijse/Crop_monitoring_system_backend/Entity/LogEntity.java
@@ -0,0 +1,53 @@
+package lk.ijse.Crop_monitoring_system_backend.Entity;
+
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.sql.Date;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "log")
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class LogEntity {
+    @Id
+    private String logId;
+    private String logDetails;
+    private Date date;
+    private String status;
+    @Column(columnDefinition = "LONGTEXT")
+    private String image2;
+
+    @ManyToMany
+    @JoinTable(
+            name = "staff_logs_details",
+            joinColumns = @JoinColumn(name = "log_id"),
+            inverseJoinColumns = @JoinColumn(name = "staff_id")
+    )
+    @JsonManagedReference // Manage the serialization
+    private Set<StaffEntity> staffLogs = new HashSet<>();
+    @ManyToMany
+    @JoinTable(
+            name = "field_logs",
+            joinColumns = @JoinColumn(name = "log_id"),
+            inverseJoinColumns = @JoinColumn(name = "field_id")
+    )
+    @JsonManagedReference // Manage the serialization
+    private Set<FieldEntity> fieldLogs = new HashSet<>();
+
+    @ManyToMany
+    @JoinTable(
+            name = "crop_logs",
+            joinColumns = @JoinColumn(name = "log_id"),
+            inverseJoinColumns = @JoinColumn(name = "crop_id")
+    )
+    @JsonManagedReference // Manage the serialization
+    private Set<CropEntity> cropLogs = new HashSet<>();
+
+}
