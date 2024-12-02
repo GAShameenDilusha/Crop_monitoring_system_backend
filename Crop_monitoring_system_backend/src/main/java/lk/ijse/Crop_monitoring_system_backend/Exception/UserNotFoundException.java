861f18f9fe2475bb0c3185449db1965838e0406e
shameen <shameendilusha5678@gmail.com>
2024-12-02 12:51:43 +0530

    update UserNotFoundException.java

---
+++ Crop_monitoring_system_backend/src/main/java/lk/ijse/Crop_monitoring_system_backend/Exception/UserNotFoundException.java
@@ -0,0 +1,15 @@
+package lk.ijse.Crop_monitoring_system_backend.Exception;
+
+public class UserNotFoundException extends RuntimeException{
+    public UserNotFoundException() {
+        super();
+    }
+
+    public UserNotFoundException(String message) {
+        super(message);
+    }
+
+    public UserNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
